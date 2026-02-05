4847dfc413b4afda914cdc0f73de12aa18d10e19
larz.parba@gmail.com <larz.parba@gmail.com>
2026-02-05 12:40:53 +0800

    intial files

---
+++ app/Http/Controllers/Api/PingController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class PingController extends Controller
+{
+    //
+    public function __invoke(): JsonResponse
+    {
+        return response()->json([
+            'status' => 'ok'
+        ]);
+    }
+}
